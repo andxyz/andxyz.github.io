82fae80896d6a8843621b0d787583aeb2702277f
Andrew Stevens <andxyz.stevens@gmail.com>
2026-03-01 00:37:46 -0500

    Regenerate main.css with modern libs

--- preview.sh
+++ preview.sh
@@ -4,6 +4,13 @@ set -x
 set -e
 
 bundle exec jekyll clean
-# sassc --style compact sass/screen.scss > stylesheets/main.css
-grass sass/screen.scss stylesheets/main.css 
+
+# sassc --style compact sass/screen.scss > sass/main_unmin.css
+
+# cargo install grass
+grass sass/screen.scss sass/main_unmin.css 
+
+# npm install lightningcss-cli
+npx lightningcss --minify --output-file stylesheets/main.css sass/main_unmin.css
+
 bundle exec jekyll serve
